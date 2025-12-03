f4d1932030f16319d309fa80e9c5a69486af7e9c
Evan Stade <evanstade@microsoft.com>
2025-12-03 22:59:09 +0000

    More direct message.

--- idb-txn-scopes/script.js
+++ idb-txn-scopes/script.js
@@ -1,12 +1,5 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
 let stopRecursing = {};
+let recurseCount = 0;
 
 const customerData = [
   { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
@@ -17,76 +10,71 @@ var createWriteAndRead = function(dbName) {
   const request = indexedDB.open(dbName, 2);
 
   request.onerror = (event) => {
-    // Handle errors.
+    console.error("Database error: ", event);
   };
   request.onsuccess = (event) => {
     const db = event.target.result;
-    if (db.name == 'db1') {
-      writeSomeOtherData(db);
+    if (db.name === 'db1') {
+      writeToProviderStore(db);
     }
-    writeCustomerData(db);
+    writeToCustomerStore(db);
     read444(db);
   }
   request.onupgradeneeded = (event) => {
     const db = event.target.result;
     db.createObjectStore("providers", { keyPath: "ssn" });
-    // Create an objectStore to hold information about our customers. We're
-    // going to use "ssn" as our key path because it's guaranteed to be
-    // unique - or at least that's what I was told during the kickoff meeting.
-    const objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
-
-    // Create an index to search customers by name. We may have duplicates
-    // so we can't use a unique index.
-    objectStore.createIndex("name", "name", { unique: false });
-
-    // Create an index to search customers by email. We want to ensure that
-    // no two customers have the same email, so use a unique index.
-    objectStore.createIndex("email", "email", { unique: true });
+    db.createObjectStore("customers", { keyPath: "ssn" });
   }
 }
 
-// Writes data to "providers" object store.
-var writeSomeOtherData = function(db) {
+var writeToProviderStore = function (db) {
   const txn = db.transaction("providers", "readwrite");
   const providerObjectStore = txn.objectStore("providers");
   customerData.forEach((customer) => {
-    providerObjectStore.add(customer);
+    providerObjectStore.put(customer);
   });
   var recurse = function(event) {
     if (!stopRecursing[db.name]) {
       var innerRequest = providerObjectStore.get("555-55-5555");
       innerRequest.onsuccess = recurse;
+      innerRequest.onerror = (event) => {
+        console.error("Database error: ", event);
+      };
+      if (++recurseCount == 1000) {
+        stopRecursing[db.name] = true;
+      }
     }
   };
   var request = providerObjectStore.get("555-55-5555");
   request.onsuccess = recurse;
+  request.onerror = (event) => {
+    console.error("Database error: ", event);
+  };
 }
 
-// Writes data to "customers" object store.
-var writeCustomerData = function(db) {
+var writeToCustomerStore = function (db) {
   const customerObjectStore = db
     .transaction("customers", "readwrite")
     .objectStore("customers");
   customerData.forEach((customer) => {
-    customerObjectStore.add(customer);
+    customerObjectStore.put(customer);
   });
 }
 
-// Reads data from "providers" object store.
 var read444 = function(db) {
   const transaction = db.transaction(["customers"]);
   const objectStore = transaction.objectStore("customers");
   const request = objectStore.get("444-44-4444");
-  request.onerror = (event) => {
-    // Handle errors!
-  };
   request.onsuccess = (event) => {
-    // Do something with the request.result!
-    console.log(`Name for SSN 444-44-4444 is ${request.result.name}`);
     stopRecursing[db.name] = true;
-    document.getElementById('result-' + db.name).innerText = "success";
+    let resultDiv = document.getElementById('result-' + db.name);
+    resultDiv.classList.remove('lds-ring');
+    if (recurseCount >= 1000) {
+      document.getElementById('timed-out').hidden = false;
+    } else {
+      document.getElementById('finished').hidden = false;
+    }
   };
 }
 
 createWriteAndRead("db1");
-createWriteAndRead("db2");
